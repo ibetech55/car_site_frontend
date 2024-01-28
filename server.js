fa5adb31461f93f91260284e3877f98ac24b37a9
ibetech55 <ibetech55@gmail.com>
2024-01-28 04:03:42 -0300

    Adjustment s to home page

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ app.use(history());
 
 app.use("/", express.static('./dist'));
 
-let port = 3000;
+let port = process.env.PORT || 3000 ;
 app.listen(port, () => console.log("Listening to Port:", port));
