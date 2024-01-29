b913305c55708d7d2d3454e753e91f2e822cc4ab
pibewiro <pibewiro@yahoo.com>
2024-01-29 20:44:28 -0300

    feat: sign in integration

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 /* eslint-disable no-undef */
 import express from 'express';
-import path from 'path'
 import history from 'connect-history-api-fallback'
 const app = express();
 app.use(history());
 
-app.use("/", path.join(__dirname, "dist"));
+app.use("/", './dist');
 
 let port = process.env.PORT || 3000 ;
 app.listen(port, () => console.log("Listening to Port:", port));
