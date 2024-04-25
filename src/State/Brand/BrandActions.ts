e231b6c435c1a08905e24b59d20209a67c458f08
pibewiro <pibewiro@yahoo.com>
2024-04-25 20:31:05 -0300

    adjust: add params active true to get car make list endpoint

--- src/State/Brand/BrandActions.ts
+++ src/State/Brand/BrandActions.ts
@@ -6,32 +6,33 @@ import { VITE_BRAND_API_URL } from "../../Configs/EnviromentVariables";
 import { GetModelsByMakeIdDto } from "../../Data/BrandDtos/GetModelsByIdDto";
 
 export const getMakeList = createAsyncThunk(
-    "brand/getMakeList",
-    async (_, { rejectWithValue }): Promise<GetMakeListDto[]> => {
-      try {
-        const { data } = await axios.get(`${VITE_BRAND_API_URL}/make/carList`);
-        return data;
-      } catch (error: any) {
-        return rejectWithValue(
-          error.response.data.message
-        ) as unknown as GetMakeListDto[];
-      }
+  "brand/getMakeList",
+  async (_, { rejectWithValue }): Promise<GetMakeListDto[]> => {
+    try {
+      const { data } = await axios.get(`${VITE_BRAND_API_URL}/make/carList`, {
+        params: { active: true },
+      });
+      return data;
+    } catch (error: any) {
+      return rejectWithValue(
+        error.response.data.message
+      ) as unknown as GetMakeListDto[];
     }
-  );
+  }
+);
 
-  export const getModelsByMakeId = createAsyncThunk(
-    "brand/getModelsByMakeId",
-    async (id: string, { rejectWithValue }): Promise<GetModelsByMakeIdDto[]> => {
-      try {
-        const { data } = await axios.get(`${VITE_BRAND_API_URL}/model/modelsByMakeId/${id}`);
-        return data;
-      } catch (error: any) {
-        return rejectWithValue(
-          error.response.data.message
-        ) as unknown as GetModelsByMakeIdDto[];
-      }
+export const getModelsByMakeId = createAsyncThunk(
+  "brand/getModelsByMakeId",
+  async (id: string, { rejectWithValue }): Promise<GetModelsByMakeIdDto[]> => {
+    try {
+      const { data } = await axios.get(
+        `${VITE_BRAND_API_URL}/model/modelsByMakeId/${id}`
+      );
+      return data;
+    } catch (error: any) {
+      return rejectWithValue(
+        error.response.data.message
+      ) as unknown as GetModelsByMakeIdDto[];
     }
-  );
-
-
-  
+  }
+);
